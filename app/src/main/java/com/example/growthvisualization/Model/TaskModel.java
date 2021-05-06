3290e66d4ae125889938824df990647d92fcc6d2
murakami-satoshi <28719644+gatti0511@users.noreply.github.com>
2021-05-06 13:56:39 +0900

    change Email in Android Studio

--- app/src/main/java/com/example/growthvisualization/Model/TaskModel.java
+++ app/src/main/java/com/example/growthvisualization/Model/TaskModel.java
@@ -9,7 +9,7 @@ public class TaskModel extends RealmObject {
     long start;
     long end;
     // カテゴリ
-    // TODO:カテゴリのまとめ方。「タスク名」でまとめられるようにする？未定
+    // TODO:カテゴリのまとめ方。「タスク名」でまとめられるようにする？まだ未定
     String category;
 
     public TaskModel() {
